69ed924ae4a1954981e57bb4524befd80794f9e0
CjShu <az937182@gmail.com>
2016-07-15 23:13:07 +0800

    1

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -60,6 +60,6 @@ using System.Runtime.InteropServices;
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.52.7.13")]
-[assembly: AssemblyFileVersion("1.52.7.13")]
+[assembly: AssemblyVersion("1.52.7.15")]
+[assembly: AssemblyFileVersion("1.52.7.15")]
 [assembly: NeutralResourcesLanguage("en")]
